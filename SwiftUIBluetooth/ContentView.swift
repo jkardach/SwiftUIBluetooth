0f2bcdc637f58735f4adcf69d4ef51475b410ce1
Jim Kardach <jim@kardach.clom>
2023-10-04 11:55:36 -0700

    Initial Commit

---
+++ SwiftUIBluetooth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIBluetooth
+//
+//  Created by jim kardach on 10/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIBluetooth/SwiftUIBluetoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBluetoothApp.swift
+//  SwiftUIBluetooth
+//
+//  Created by jim kardach on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBluetoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
