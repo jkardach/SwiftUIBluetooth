aa15c4e9470d92eb61a39ab83c86fd5311b50983
Jim Kardach <jim@kardach.clom>
2023-10-08 09:04:28 -0700

    Scanning Bluetooth radio project

---
+++ SwiftUIBluetooth/BLEManager.swift
@@ -0,0 +1,8 @@
+//
+//  BLEManager.swift
+//  SwiftUIBluetooth
+//
+//  Created by jim kardach on 10/4/23.
+//
+
+import Foundation
---
+++ SwiftUIBluetooth/DeviceView.swift
@@ -0,0 +1,18 @@
+//
+//  DeviceView.swift
+//  SwiftUIBluetooth
+//
+//  Created by jim kardach on 10/4/23.
+//
+
+import SwiftUI
+
+struct DeviceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeviceView()
+}
